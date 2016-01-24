0b93675e18f3a1d473e179f8f66047956c70cd5d
Kiryanenko <KiryanenkoAV@mail.ru>
2016-01-24 17:01:05 +0300

    add settings

--- keyboard_lamp.ino
+++ keyboard_lamp.ino
@@ -1,8 +1,22 @@
-#define INT_NUM 1		// прерывания с номерами 0 (на digital pin 2) и 1 (на digital pin 3)
+#define RED_POT_PIN		A7	// пин красного рычага
+#define GREEN_POT_PIN	A6	// пин зеленого рычага
+#define BLUE_POT_PIN	A5	// пин синего рычага
 
-volatile bool isRand;	// прервание: изменение режима работы
+#define RED_LED			6	// пин красных светодиодов
+#define GREEN_LED		2	// пин красных светодиодов
+#define BLUE_LED		4	// пин красных светодиодов
 
-void int_set_mode() {	// прервание: изменение режима работы
+#define DURATION_PIN	A6	// пин рычага, отвечающий за длительность удерживания цвета
+#define SHADING_PIN		A5	// пин рычага, отвечающий за длительность перехода
+
+#define K_DURATION		300	// макс длительность удерживания цвета в сек
+#define K_SHADING		30	// макс длительность перехода в сек
+
+#define INT_NUM			1	// прерывания с номерами 0 (на digital pin 2) и 1 (на digital pin 3)
+
+volatile bool isRand;		// прервание: изменение режима работы
+
+void int_set_mode() {		// прервание: изменение режима работы
 	isRand = !isRand;
 }
 
@@ -14,7 +28,4 @@ void setup()
 
 void loop()
 {
-
-  /* add main program code here */
-
 }
