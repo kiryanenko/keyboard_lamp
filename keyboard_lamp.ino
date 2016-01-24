eedd00059a1eeee11f5f27308121b44335f0a46b
Kiryanenko <KiryanenkoAV@mail.ru>
2016-01-24 15:47:09 +0300

    add interrupt

--- keyboard_lamp.ino
+++ keyboard_lamp.ino
@@ -1,7 +1,14 @@
+#define INT_NUM 1		// прерывания с номерами 0 (на digital pin 2) и 1 (на digital pin 3)
+
+volatile bool isRand;	// прервание: изменение режима работы
+
+void int_set_mode() {	// прервание: изменение режима работы
+	isRand = !isRand;
+}
+
 void setup()
 {
-
-  /* add setup code here */
+	attachInterrupt(INT_NUM, int_set_mode, RISING); // привязываем прерывание к функции int_set_mode().
 
 }
 
